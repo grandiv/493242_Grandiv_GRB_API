706c408cfd3ee2333e576ce496f737f7a433085f
Muhammad Grandiv <grandivfarand@gmail.com>
2024-06-01 12:05:07 +0700

    test vercel lagi

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "TBDatabase",
-  password: "Vidnarg1337",
-  port: 5432,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
