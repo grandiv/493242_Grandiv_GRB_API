0ba24d848d0b4ed8a3fcaf07ed203d782a4f71a0
Muhammad Grandiv <grandivfarand@gmail.com>
2024-06-01 23:11:33 +0700

    vercel

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
